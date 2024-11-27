1bf71762bb8edba138652b4f00f06a3ddc149f6f
sandeep <thadigotlasandeepkumar@gmail.com>
2024-11-27 16:49:49 +0530

    all

---
+++ day4-target/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "dependent" {
+
+    ami="ami-0614680123427b75e"
+    instance_type = "t2.micro"
+    key_name = "apsouth-1key"
+
+}
+resource "aws_s3_bucket" "dependent" {
+     bucket = "ysgdugdusagd"
+}
