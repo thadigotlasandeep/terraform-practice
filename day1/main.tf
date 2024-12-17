f6d7c67fc96023c0352ffe52da795457a2a0ae3b
sandeep <thadigotlasandeepkumar@gmail.com>
2024-12-17 16:17:14 +0530

    all

--- day1/main.tf
+++ day1/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
  ami = "ami-0fd05997b4dff7aac"
- instance_type ="t2.micro"
+ instance_type ="t2.medium"
  #key_name = "Proceed without a key pair"
  tags = {
   name = "rishi"
